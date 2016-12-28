db94a7b3098894cf7b6e6e515e051e215655ae32
wesbasinger <wbasinger@villagetechschools.org>
2016-12-27 23:45:49 -0600

    Fetching data from server quite nicely...

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
 
 var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      ready: false,
+      score: 0,
+      name: null,
+      highScore: null,
+      tweets: null
+    }
+  },
+
+  componentDidMount() {
+    $.ajax({
+      url: 'api/tweets',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({tweets: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }.bind(this)
+    });
+  },
+
   render: function() {
-    return(
-      <div>This is a React component</div>
-    )
+    if (!this.state.ready) {
+      return(
+        <div>
+          <h1>Are you ready to play?</h1>
+          <button>Yes</button>
+        </div>
+      )
+    } else {
+      return(
+        <div>You must be ready!</div>
+      )
+    }
   }
 });
 
